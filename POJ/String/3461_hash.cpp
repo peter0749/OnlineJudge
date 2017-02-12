18ecbff3c79c141e400dc9956c784f0f2db233e1
Kuang-Yu Jeng <peter0749@users.noreply.github.com>
2017-02-12 12:10:13 +0800

    Update 3461_hash.cpp

--- POJ/String/3461_hash.cpp
+++ POJ/String/3461_hash.cpp
@@ -4,15 +4,16 @@
 #include <cstdlib>
 #include <cstring>
 #define STRL 1000010
+#define PURGE(X) sscanf( (X) , "%s" , (X) )
 using namespace std;
 typedef unsigned long long int UT;
-string str, pat;
+char str[STRL], pat[STRL];
 const UT hsaBase = 37, MOD1d7=1000000007;
 UT hsaPow, hsaS, hsaP;
 
-UT gethash(string &S) {
+UT gethash(char *S, int len) {
     UT h=0;
-    for(string::size_type i=0; i!=pat.length(); ++i ) {
+    for(int i=0; i!=len; ++i ) {
         h = (h * hsaBase  ) % MOD1d7;
         h = (h + (UT)S[i]) % MOD1d7;
     }
@@ -20,9 +21,11 @@ UT gethash(string &S) {
 }
 
 int solv() {
-    if(pat.length()>str.length()) return 0;
+    int plen=strlen(pat);
+    int slen=strlen(str);
+    if(plen>slen) return 0;
     hsaPow = 1uLL;
-    unsigned int pol = pat.length()-1; //highest power
+    unsigned int pol = plen-1; //highest power
     UT temp = hsaBase;
     //Initialize fast-pow
     while(pol) {
@@ -30,24 +33,27 @@ int solv() {
         temp = (temp * temp) % MOD1d7;
         pol>>=1;
     }
-    hsaS = gethash(str); hsaP = gethash(pat);
+    hsaS = gethash(str, plen); hsaP = gethash(pat, plen);
     int ans = 0;
-    for(string::size_type i=0 ;; ++i) {
+    for(int i=0 ;; ++i) {
         if(hsaS==hsaP) ++ans;
-        if(i+pat.length()==str.length()) break; //Warn
+        if(i+plen==slen) break; //Warn
         hsaS = (hsaS-((UT)str[i]*hsaPow)%MOD1d7+MOD1d7) % MOD1d7;
-        hsaS = ( (hsaS*hsaBase)%MOD1d7 + (UT)str[i+pat.length()] ) % MOD1d7;
+        hsaS = ( (hsaS*hsaBase)%MOD1d7 + (UT)str[i+plen] ) % MOD1d7;
     }
     return ans;
 }
 
 int main(void) {
-    ios::sync_with_stdio(false);cin.tie(0);
     int T;
-    cin >> T;
+    fgets(str, sizeof(str), stdin);
+    sscanf(str, "%d", &T);
     while(T-- ) {
-        cin >> pat >> str;
-        cout << solv() << '\n';
+        fgets(pat, sizeof(pat), stdin);
+        fgets(str, sizeof(str), stdin);
+        PURGE(pat);
+        PURGE(str);
+        printf("%d\n",solv());
     }
     return 0;
 }
