8f6dbbb6ef224d6726a7f907bf1f23ec896df49e
Kuang-Yu Jeng <peter0749@users.noreply.github.com>
2017-03-16 11:26:57 +0800

    Update Uva10003_CuttingSticks.cpp

--- Uva/Uva10003_CuttingSticks.cpp
+++ Uva/Uva10003_CuttingSticks.cpp
@@ -6,16 +6,16 @@ const int LIM=1e9+7;
 int dp[MAXN][MAXN];
 
 int cuts(int l, int r) {
-    if(r-l<=1) return 0; //no cut required
-    if(dp[l][r]!=-1) return dp[l][r];
-    int cost=cutp.at(r)-cutp.at(l);
-    int minv=LIM;
-    for(int i=l+1; i<r; ++i) {
-        int t = cuts(l,i)+cuts(i,r);
-        minv=min(minv, t);
+    for(int i=r-2; i>=0; --i) {
+        for(int j=i+2; j<=r; ++j) {
+            int minv=LIM;
+            for(int k=i+1; k<j; ++k) {
+                minv=min(minv,dp[i][k]+dp[k][j]);
+            }
+            dp[i][j]=cutp.at(j)-cutp.at(i)+minv;
+        }
     }
-    cost+=minv;
-    return dp[l][r]=cost;
+    return dp[l][r];
 }
 
 int main(void) {
@@ -31,7 +31,7 @@ int main(void) {
         }
         cutp.push_back(l);
         int ans=LIM;
-        for(int i=0; i<=n+1; ++i) for(int j=0; j<=n+1; ++j) dp[i][j]=-1;
+        for(int i=0; i<=n+1; ++i) for(int j=0; j<=n+1; ++j) dp[i][j]=0;
         cout<< "The minimum cutting is " << cuts(0,n+1) <<".\n";
     }
     return 0;
