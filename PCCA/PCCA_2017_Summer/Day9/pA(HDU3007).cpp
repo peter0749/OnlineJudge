a08a665b270fb100bd8e468381fca339fc9205d3
jengku <jengku@gmail.com>
2017-07-13 23:39:27 +0800

    foo

--- PCCA/PCCA_2017_Summer/Day9/pA(HDU3007).cpp
+++ PCCA/PCCA_2017_Summer/Day9/pA(HDU3007).cpp
@@ -10,9 +10,38 @@
 #include <cstdlib>
 #include <cstring>
 #include <cmath>
+#include <complex>
+#include <valarray>
+
 using namespace std;
 
-vector< pair<double,double> > cord;
+typedef pair<double,double> PII;
+#define X first
+#define Y second
+vector< PII > cord;
+
+double crossP(PII &o, PII &a, PII &b) {
+    return (a.X - o.X) * (b.Y-o.Y) - \
+           (a.Y - o.Y) * (b.X-o.X);
+}
+
+void Andrew_removeInnerPoints(void) {
+    vector< PII > convex_hull;
+    sort(cord.begin(), cord.end());
+    for (int i=0; i<cord.size(); ++i) {
+        while(convex_hull.size()>=2 && crossP(convex_hull[convex_hull.size()-2], convex_hull[convex_hull.size()-1], cord[i]) <= 0.0 ) convex_hull.pop_back();
+        convex_hull.push_back(cord[i]);
+    }
+    for (int i=cord.size()-2, t=convex_hull.size()+1; i>=0; --i) {
+        while(convex_hull.size()>=t && crossP(convex_hull[convex_hull.size()-2], convex_hull[convex_hull.size()-1], cord[i]) <= 0.0 ) convex_hull.pop_back();
+        convex_hull.push_back(cord[i]);
+    }
+    convex_hull.pop_back();
+    cord.clear();
+    cord.reserve(convex_hull.size());
+    cord = convex_hull;
+    convex_hull.clear();
+}
 
 inline double l2(double x, double y) {
     double maxv = 0;
@@ -37,15 +66,14 @@ inline double l1(double x, double y) {
 pair< pair<double, double> , double> brute(int ite, int early_stop=1e9) {
     double initx, inity;
     double eps = 1;
-    //double eps = 1e-3;
     double momentum = 0.9;
     double delta = 0.95;
     //double delta = 1;
-    //int idx = cord.size()/2;
-    //initx = cord[idx].first;
-    //inity = cord[idx].second;
-    initx = ((double)rand()*2e10/(double)RAND_MAX)-1e10;
-    inity = ((double)rand()*2e10/(double)RAND_MAX)-1e10;
+    int idx = cord.size()/2;
+    initx = cord[idx].first;
+    inity = cord[idx].second;
+    //initx = ((double)rand()*2e10/(double)RAND_MAX)-1e10;
+    //inity = ((double)rand()*2e10/(double)RAND_MAX)-1e10;
 
     double ori=1e9;
     double min_loss = ori;
@@ -84,8 +112,13 @@ int main(void) {
             sscanf(buff,"%lf%lf", &x, &y);
             cord.push_back(make_pair(x,y));
         }
-        pair< pair<double,double> , double> res = brute(1010, 200);
-        printf("%.2lf %.2lf %.2lf\n" , res.first.first, res.first.second, res.second);
+        if (N>10)
+            Andrew_removeInnerPoints();
+        pair< pair<double,double> , double> res = brute(2001);
+        if (abs(res.first.first)<0.005) res.first.first = 0.0;
+        if (abs(res.first.second)<0.005) res.first.second = 0.0;
+        if (abs(res.second)<0.005) res.second = 0.0;
+        printf("%.2f %.2f %.2f\n" , res.first.first, res.first.second, res.second);
     }
 
     return 0;
