36610f6d452758a0b6ac11e822784a3af4655ed3
jengku <jengku@gmail.com>
2017-07-18 11:15:09 +0800

    foo

--- ZeroJudge/b278.cpp
+++ ZeroJudge/b278.cpp
@@ -1,96 +1,99 @@
+#pragma GCC target ("avx")
+#pragma GCC optimize ("Os")
 #include <bits/stdc++.h>
 using namespace std;
 
 struct Node {
-    long long int val;    // 用於修改、更新的值
-    int lc, rc; // 左右子樹的 index
+    int lc, rc;
+    long long val;
 };
 
 struct SegmentTreeP {
-    enum {QUERY_NUM=510101, TREENN=8200000};
-    int root[QUERY_NUM]; // 為了方便，寫死就好，這用來記錄線段樹的更新 root
-    int node_num;
-    Node node[TREENN];
-
-    SegmentTreeP() : node_num(0) {}
-    
-    int build(int l, int r) { // 傳回更新節點指標（不是指記憶體指標）
+    enum { MAXNODE=6101010, MAXREC=500010};
+    int nodeNum;
+    int his[MAXREC];
+    Node sg[MAXNODE];
+    SegmentTreeP() : nodeNum(0) { }
+    int build(int l, int r) {
         if (l>r) return 0; // illegal
-        int new_node = node_num++;
-        node[new_node].lc = node[new_node].rc = node[new_node].val = 0;
-        if (l==r) return new_node;
-        int m = l+((r-l)>>1); 
-        node[new_node].lc = build(l, m);
-        node[new_node].rc = build(m+1, r);
-        return new_node;
+        int k = nodeNum++; // allocate new node
+        sg[k].lc = sg[k].rc = sg[k].val = 0;
+        if (l==r) return k;
+        int m = l+((r-l)>>1);
+        sg[k].lc = build(l, m);
+        sg[k].rc = build(m+1, r);
+        return k;
     }
-    void init_build(int l, int r) {
-        root[0] = build(l, r);
+    void init(int N) {
+        memset(his,0x00,sizeof(his[0])*MAXREC);
+        memset(sg, 0x00,sizeof(sg[0])*MAXNODE);
+        nodeNum = 0;
+        his[0] = build(1,N);
     }
-    int update(int acs, int l, int r, int q, int val) {
-        int new_node = node_num++;
-        node[new_node] = node[acs];
-        if (l==r) {
-            node[new_node].val = val;
-            return new_node;
+    int update(int acs, int l, int r, int q, long long val) {
+        if (l>r) return 0;
+        int k = nodeNum++;
+        sg[k] = sg[acs];   // new node derived from acs
+        if (l==r) { // modify
+            sg[k].val = val;
+            return k;
         }
         int m = l+((r-l)>>1);
-        if (q <= m)
-            node[new_node].lc = update(node[acs].lc, l, m, q, val);
+        if (q<=m) 
+            sg[k].lc = update(sg[acs].lc, l, m, q, val);// update left
         else 
-            node[new_node].rc = update(node[acs].rc,m+1,r, q, val);
-        node[new_node].val = node[node[new_node].lc].val + \
-                             node[node[new_node].rc].val;
-        return new_node;
+            sg[k].rc = update(sg[acs].rc, m+1, r, q, val);// update right
+        sg[k].val= sg[sg[k].lc].val + sg[sg[k].rc].val; // update
+        return k;
     }
-    long long int query(int acs, int l, int r, int ql, int qr) {
-        if (r<ql || l>qr) return 0LL;
-        if (l>=ql && r<=qr) return node[acs].val;
+    long long query(int l, int r, int ql, int qr, int q) {
+        if (l>r || l>qr || r < ql) return 0LL; // illegal
+        if (l>=ql && r<=qr) { // in range
+            return sg[q].val;
+        }
         int m = l+((r-l)>>1);
-        long long int sum = 0;
-        if (ql <= m)
-            sum += query(node[acs].lc,  l, m, ql, qr);
-        if (m  < qr)
-            sum += query(node[acs].rc,m+1, r, ql, qr);
-        return sum;
+        long long res = 0;
+        if (ql<=m)
+            res += query(l, m, ql, qr, sg[q].lc);
+        if (m< qr)
+            res += query(m+1, r, ql, qr, sg[q].rc);
+        return res;
     }
 };
 
-SegmentTreeP *st;
-
 int main(void) {
-    int N, M;
-    st = NULL;
-    st = new SegmentTreeP;
-    assert(st!=NULL);
-    SegmentTreeP &Solver = *st;
-    while (scanf("%d%d",&N,&M)==2) {
-        Solver.init_build(1, N);
-        int preVer=0, op, l, r;
-        for (int i=1; i<=M; ++i) {
-            scanf("%d%d",&op, &l);
-            switch (op) {
+    SegmentTreeP *newTree = NULL;
+    newTree = new SegmentTreeP();
+    assert(newTree!=NULL);
+    SegmentTreeP &tree = *newTree;
+    int N, Q;
+    while(scanf("%d%d", &N, &Q)==2) {
+        tree.init(N);
+        int preVer = 0;
+        for (int i=1; i<=Q; ++i) {
+            int op, l, r;
+            scanf("%d%d", &op, &l);
+            switch(op) {
                 case 0: {
-                    int rec = max(0,i-1-l);
-                    Solver.root[i] = Solver.root[rec];
-                    preVer = i;
+                            int rec = max(0, i-l-1);
+                            tree.his[i] = tree.his[rec];
+                            preVer = i;
                         }
-                    break;
-                case 1: {
-                    scanf("%d", &r);
-                    Solver.root[i] = Solver.update(Solver.root[preVer], 1, N, l, r);
-                    preVer = i;
-                        }
-                    break;
-                case 2: {
-                    scanf("%d", &r);
-                    printf("%lld\n", Solver.query(Solver.root[preVer], 1, N, l, r));
-                    Solver.root[i] = Solver.root[preVer];
-                    preVer = i;
-                        }
-                    break;
+                        break;
+                case 1:
+                        scanf("%d", &r);
+                        tree.his[i] = tree.update(tree.his[preVer], 1,N,l,r);
+                        preVer = i;
+                        break;
+                case 2:
+                        scanf("%d", &r);
+                        printf("%lld\n", tree.query(1,N,l,r,tree.his[preVer]));
+                        tree.his[i] = tree.his[preVer];
+                        preVer = i;
+                        break;
             }
         }
     }
+    delete newTree;
     return 0;
 }
