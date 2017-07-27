80ce9606d52baf94715ce2f99b41bf3d0412c715
jengku <jengku@gmail.com>
2017-07-27 12:45:45 +0800

    foo

--- ZeroJudge/a577_高精度乘法.cpp
+++ ZeroJudge/a577_高精度乘法.cpp
@@ -1,3 +1,6 @@
+#pragma GCC target ("avx")
+#pragma GCC optimize ("Os")
+#pragma GCC optimize ("O3")
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -10,8 +13,10 @@
 namespace FFT  // WARNING!!! do not reveal this namespace
 {
     using namespace std; // be careful
-    const int MaxFastBits = 16;
-    int **gFFTBitTable = 0;
+#ifdef M_PIl
+#undef M_PIl
+#endif
+#define M_PIl 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
 
     int NumberOfBitsNeeded(int PowerOfTwo) {
         for (int i = 0;; ++i) {
@@ -21,37 +26,25 @@ namespace FFT  // WARNING!!! do not reveal this namespace
         }
     }
 
-    int ReverseBits(int index, int NumBits) {
-        int ret = 0;
-        for (int i = 0; i < NumBits; ++i, index >>= 1) {
-            ret = (ret << 1) | (index & 1);
-        }
-        return ret;
+    inline uint32_t FastReverseBits(uint32_t a, int NumBits) {
+        a = ( ( a & 0x55555555U ) << 1 ) | ( ( a & 0xAAAAAAAAU ) >> 1 ) ;
+        a = ( ( a & 0x33333333U ) << 2 ) | ( ( a & 0xCCCCCCCCU ) >> 2 ) ;
+        a = ( ( a & 0x0F0F0F0FU ) << 4 ) | ( ( a & 0xF0F0F0F0U ) >> 4 ) ;
+        a = ( ( a & 0x00FF00FFU ) << 8 ) | ( ( a & 0xFF00FF00U ) >> 8 ) ;
+        a = ( ( a & 0x0000FFFFU ) << 16 ) | ( ( a & 0xFFFF0000U ) >> 16 ) ;
+        return a >> (32 - NumBits);
     }
 
-    void InitFFT() {
-        gFFTBitTable = new int *[MaxFastBits];
-        for (int i = 1, length = 2; i <= MaxFastBits; ++i, length <<= 1) {
-            gFFTBitTable[i - 1] = new int[length];
-            for (int j = 0; j < length; ++j) {
-                gFFTBitTable[i - 1][j] = ReverseBits(j, i);
-            }
-        }
-    }
-    inline int FastReverseBits(int i, int NumBits) {
-        return NumBits <= MaxFastBits ? gFFTBitTable[NumBits - 1][i] : ReverseBits(i, NumBits);
-    }
-
-    void FFT(bool InverseTransform, vector<complex<double> >& In, vector<complex<double> >& Out) {
-        if (!gFFTBitTable) { InitFFT(); }
+    void FFT(bool, const vector<complex<double> >&, vector<complex<double> >&) __attribute__((optimize("fast-math")));
+    void FFT(bool InverseTransform, const vector<complex<double> >& In, vector<complex<double> >& Out) {
         // simultaneous data copy and bit-reversal ordering into outputs
         int NumSamples = In.size();
         int NumBits = NumberOfBitsNeeded(NumSamples);
         for (int i = 0; i < NumSamples; ++i) {
-            Out[FastReverseBits(i, NumBits)] = In[i];
+            Out[FastReverseBits((uint32_t)i, NumBits)] = In[i];
         }
         // the FFT process
-        double angle_numerator = acos(-1.) * (InverseTransform ? -2 : 2);
+        double angle_numerator = M_PIl * (InverseTransform ? -2 : 2);
         for (int BlockEnd = 1, BlockSize = 2; BlockSize <= NumSamples; BlockSize <<= 1) {
             double delta_angle = angle_numerator / BlockSize;
             double sin1 = sin(-delta_angle);
@@ -79,7 +72,10 @@ namespace FFT  // WARNING!!! do not reveal this namespace
         }
     }
 
-    vector<double> convolution(vector<double> a, vector<double> b) {
+    template<class T>
+    void convolution(const vector<T> &a, const vector<T> &b, vector<double> &ret) __attribute__((optimize("fast-math")));
+    template<class T>
+    void convolution(const vector<T> &a, const vector<T> &b, vector<double> &ret) {
         int n = a.size();
         vector<complex<double> > s(n), d1(n), d2(n), y(n);
         for (int i = 0; i < n; ++i) {
@@ -95,16 +91,12 @@ namespace FFT  // WARNING!!! do not reveal this namespace
             y[i] = d1[i] * d2[i];
         }
         FFT(true, y, s);
-        vector<double> ret(n);
+        ret.resize(n,0);
         for (int i = 0; i < n; ++i) {
             ret[i] = s[i].real();
         }
-        return ret;
     }
 };
-
-const size_t MAXN=1<<18;
-uint8_t a[MAXN], b[MAXN];
 
 int main(void) {
     using namespace std;
@@ -112,25 +104,29 @@ int main(void) {
     cin.tie(0); cout.tie(0);
     const double eps=1.5e-1;
     string sa, sb;
+    vector<uint8_t>a, b;
+    vector<double> res;
+    vector<long long> res2;
     while(cin>>sa>>sb) {
-        memset(a,0x00,sizeof(a));
-        memset(b,0x00,sizeof(b));
         int maxN = sa.length()+sb.length();
         int digitN = 1;
         for (; maxN>digitN; digitN<<=1);
+        a.resize(digitN,0);
+        b.resize(digitN,0);
         for (int i=0; i<sa.length(); ++i) {
             a[i] = sa[ sa.length()-1-i ]-'0';
         }
         for (int i=digitN-sb.length(), j=0; j<sb.length(); ++i, ++j) {
             b[i] = sb[j]-'0';
         }
-        vector<double> res = FFT::convolution(vector<double>(a,a+digitN), vector<double>(b,b+digitN));
-        vector<long long> res2((size_t)maxN, 0);
+        FFT::convolution<uint8_t>(a, b, res);
+        a.clear(), b.clear();
+        res2.resize(digitN, 0);
         for (int i=1; i<=maxN; ++i) {
             res2[i-1] = ((long long)floor(res[i]+eps));
         }
         res.clear();
-        for (int i=0; i<maxN; ++i) {
+        for (int i=0; i<maxN-1; ++i) {
             res2[i+1] += res2[i]/10LL;
             res2[i  ] %= 10LL; 
         }
