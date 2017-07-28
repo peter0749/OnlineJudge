fac82c07fb42459c8a80e1083c037c5d55646ea3
jengku <jengku@gmail.com>
2017-07-28 15:08:41 +0800

    foo

--- ZeroJudge/a577_高精度乘法.cpp
+++ ZeroJudge/a577_高精度乘法.cpp
@@ -81,30 +81,18 @@ namespace FFT  // WARNING!!! do not reveal this namespace
 
     // 同上，如果編譯器不支援 fast-math 選項，記得註解掉下面兩行
     template<class T>
-        void convolution(const vector<T> &a, const vector<T> &b, vector<double> &ret) __attribute__((optimize("fast-math")));
-    // 卷積，輸入"""等長"""的 a, b 兩向量（長度必須是 2^k），會得到 a * b （a卷積b）的結果
-    // 下面套用 ZOJ 上的例子:
-    /**
-     * Given two sequences {a1, a2, a3.. an} and {b1, b2, b3... bn},
-     * their repeat convolution means:
-     * r1 = a1*b1 + a2*b2 + a3*b3 + ... + an*bn
-     * r2 = a1*bn + a2*b1 + a3*b2 + ... + an*bn-1
-     * r3 = a1*bn-1 + a2*bn + a3*b1 + ... + an*bn-2
-     * ...
-     * rn = a1*b2 + a2*b3 + a3*b4 + ... + an-1*bn + an*b1
-     * Notice n >= 2 and n must be power of 2.
-     */
+        void mul(const vector<T> &a, const vector<T> &b, vector<double> &ret) __attribute__((optimize("fast-math")));
+    // 下面的流程 A*B -> a=FFT(A); b=FFT(B) -> c = a dot b -> ANS = IFFT(c)
     template<class T>
-        void convolution(const vector<T> &a, const vector<T> &b, vector<double> &ret) {
+        void mul(const vector<T> &a, const vector<T> &b, vector<double> &ret) {
             int n = a.size();
             vector<complex<double> > s(n), d1(n), d2(n), y(n);
             for (int i = 0; i < n; ++i) {
                 s[i] = complex<double>(a[i], 0);
             }
             FFT(false, s, d1);
-            s[0] = complex<double>(b[0], 0);
-            for (int i = 1; i < n; ++i) {
-                s[i] = complex<double>(b[n - i], 0);
+            for (int i = 0; i < n; ++i) {
+                s[i] = complex<double>(b[i], 0);
             }
             FFT(false, s, d2);
             for (int i = 0; i < n; ++i) {
@@ -126,14 +114,14 @@ int main(void) {
     vector<uint8_t>a, b;
     vector<double> res;
     vector<uint32_t> res2;
-    while(scanf("%s %s",sa,sb)==2) {
+    while(scanf("%s%s",sa,sb)==2) {
         int sa_length=strlen(sa);
         int sb_length=strlen(sb);
         int maxN = sa_length+sb_length;
         // 例：123*456
         // a = [3 2 1 0 0 0]
-        // b = [0 0 0 4 5 6]
-        // 因此卷積後的 r0 都會是 0
+        // b = [6 5 4 0 0 0]
+        // a*b = IFFT(FFT(A) dot FFT(B))
         int digitN = 1;
         for (; maxN>digitN; digitN<<=1);
         a.resize(digitN,0);
@@ -141,20 +129,20 @@ int main(void) {
         for (int i=0; i<sa_length; ++i) {
             a[i] = sa[ sa_length-1-i ]-'0';
         }
-        for (int i=digitN-sb_length, j=0; j<sb_length; ++i, ++j) {
-            b[i] = sb[j]-'0';
+        for (int i=0; i<sb_length; ++i) {
+            b[i] = sb[ sb_length-1-i ]-'0';
         }
-        FFT::convolution<uint8_t>(a, b, res);
+        FFT::mul<uint8_t>(a, b, res);
+        //for (const auto &v:res) cout<<v<<endl;
         a.clear(), b.clear();
         res2.resize(digitN, 0);
-        int d = min((int)res.size()-1, (int)maxN);
-        for (int i=1; i<=d; ++i) {
-            res2[i-1] = ((uint32_t)floor(res[i]+eps));
+        for (int i=0; i<digitN; ++i) {
+            res2[i] = ((uint32_t)floor(res[i]+eps));
         }
         res.clear();
         for (int i=0; i<maxN-1; ++i) {
             res2[i+1] += res2[i]/10;
-            res2[i  ] %= 10; 
+            res2[i  ] %= 10;
         }
         bool detected=false;
         for (int i=maxN-1; i>=0; --i) {
