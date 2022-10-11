22408e64c5cca6108f2975f815ca0f3ee415a169
Kuang-Yu Jeng <peter0749@users.noreply.github.com>
2022-10-11 21:16:17 +0800

    Update 70. Climbing Stairs.cpp

--- LeetCode/70. Climbing Stairs.cpp
+++ LeetCode/70. Climbing Stairs.cpp
@@ -1,3 +1,4 @@
+// Top-down
 class Solution {
 public:
     int memory[46] = {0};
@@ -7,5 +8,18 @@ public:
         if (memory[n]!=0)
             return memory[n];
         return memory[n] = climbStairs(n-2) + climbStairs(n-1);
+    }
+};
+
+// Bottom-up
+class Solution {
+public:
+    int dp[46] = {0};
+    int climbStairs(int n) {
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i=2; i<=n; ++i)
+            dp[i] = dp[i-1] + dp[i-2];
+        return dp[n];
     }
 };
